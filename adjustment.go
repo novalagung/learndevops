cab9ab8b82b636769c88a45220f4fcb5bd7fe7e1
novalagung <caknopal@gmail.com>
2020-05-20 00:08:43 +0700

    add fb pixel

--- adjustment.go
+++ adjustment.go
@@ -105,6 +105,11 @@ func doAdjustment(isoLang string) error {
 		buttonScriptReplacement := `<script async defer src="https://buttons.github.io/buttons.js"></script>` + buttonScriptToFind
 		htmlString = strings.Replace(htmlString, buttonScriptToFind, buttonScriptReplacement, -1)
 
+		// ===== inject fb pixel
+		fbPixelToFind := `</head>`
+		fbPixelReplacement := `<script>!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1247398778924723');fbq('track','PageView');</script><noscript><imgheight="1"width="1"style="display:none"src="https://www.facebook.com/tr?id=1247398778924723&ev=PageView&noscript=1"/></noscript>` + fbPixelToFind
+		htmlString = strings.Replace(htmlString, fbPixelToFind, fbPixelReplacement, -1)
+
 		// ==== inject adjustment css
 		adjustmentCSSToFind := `</head>`
 		adjustmentCSSReplacement := `<link rel="stylesheet" href="/` + lang + `/adjustment.css?v=` + getVersion() + `">` + adjustmentCSSToFind
